6d2a629cb777756f4e2abdb46dabd2a8bf2511cc
Vinicius <viniciusrdias@hotmail.com>
2026-09-07 23:24:04 -0300

    Renderizando Quizzes da API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
 const API = 'https://mock-api.driven.com.br/api/v6/buzzquizz'
 
+carregarQuizzes();
 
 function carregarQuizzes() {
-    const promise = axios.get(`${API} + /quizzes`)
+    const promise = axios.get(API + '/quizzes')
+    promise.then(renderizarQuizzes)
+}
+
+function renderizarQuizzes(response) {
+    console.log(response.data)
+    const listaTodosQuizzes = document.querySelector(".todosOsQuizzes");
+    listaTodosQuizzes.innerHTML = "";
+
+    for(let i = 0; i < response.data.length; i++) {
+        const quizzes = response.data[i];
+
+        listaTodosQuizzes.innerHTML += `
+        <div class="quizz">
+            <img src="${quizzes.image}">
+            <span class="titulo-card">
+                ${quizzes.title}
+            </span>
+         </div>
+        `
+    }
 }
