0ffa1d407dfd1d93b45082349d701c6a7cb7a829
Vinicius <viniciusrdias@hotmail.com>
2026-09-09 20:44:56 -0300

    feat: corrigida renderizao de cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 const API = 'https://mock-api.driven.com.br/api/v6/buzzquizz'
 const CONTAINER_TELA_2 = document.querySelector(".tela2");
 
+
 let quizzEscolhido = [];
-let perguntaAtual = 0;
 let respostas = [];
-
-
+let pergunta = [];
+let perguntaAtual = 0;
+let acertos = 0;
+let perguntas = [];
 
 iniciar();
 
@@ -36,7 +38,6 @@ function renderizarQuizzes(response) {
 
 
 function escolherQuizz(id) {
-    perguntaAtual = 0;
 
     ocultarTela1();
 
@@ -49,13 +50,14 @@ function escolherQuizz(id) {
 
 
 function renderizarQuizzEscolhido(response) {
+
     quizzEscolhido = response.data;
-    respostas = quizzEscolhido.questions[perguntaAtual].answers;
-    respostas.sort(comparador)
-    console.log(response)
+
+    perguntas = quizzEscolhido.questions;
 
 
-    CONTAINER_TELA_2.innerHTML += `
+
+    CONTAINER_TELA_2.innerHTML = `
         <div class="quizz-escolhido">
             <img src="${quizzEscolhido.image}">
             <span class="titulo-card-escolhido">
@@ -63,44 +65,33 @@ function renderizarQuizzEscolhido(response) {
             </span>
         </div>
         
-        <div class="container-perguntas">
-
-            <span class="titulo">${quizzEscolhido.questions[perguntaAtual].title}</span>
-
-            <div class="container-resposta" onclick="escolherResposta(this)">
-                <img src="${quizzEscolhido.questions[perguntaAtual].answers[0].image}">
-                <span>${quizzEscolhido.questions[perguntaAtual].answers[0].text}</span>
-            </div>
-
-            <div class="container-resposta" onclick="escolherResposta(this)">
-                <img src="${quizzEscolhido.questions[perguntaAtual].answers[1].image}">
-                <span>${quizzEscolhido.questions[perguntaAtual].answers[1].text}</span>
-            </div>
-
-            <div class="container-resposta" onclick="escolherResposta(this)">
-                <img src="${quizzEscolhido.questions[perguntaAtual].answers[2].image}">
-                <span>${quizzEscolhido.questions[perguntaAtual].answers[2].text}</span>
-            </div>
-
-            <div class="container-resposta" onclick="escolherResposta(this)">
-                <img src="${quizzEscolhido.questions[perguntaAtual].answers[3].image}">
-                <span>${quizzEscolhido.questions[perguntaAtual].answers[3].text}</span>
-            </div>
-
-        </div>
     `
+    let htmlPerguntas = '';
+    
+    for (let i = 0; i < perguntas.length; i++) {
+
+        htmlPerguntas += `
+            <div class="container-perguntas">
+
+                <span class="titulo" style="background:${perguntas[i].color}">${perguntas[i].title}</span>
+
+            `
+        for (let j = 0; j < perguntas[i].answers.length; j++) {
+            htmlPerguntas += `
+                <div class="container-resposta" onclick="escolherResposta(this)">
+                    <img src="${perguntas[i].answers[j].image}">
+                    <span class="legenda">${perguntas[i].answers[j].text}</span>
+                </div>
+        `
+        }
+        htmlPerguntas += `</div>`
+        CONTAINER_TELA_2.innerHTML = htmlPerguntas;
+    }
+    
 }
 
 
-/*function proximaPergunta() {
-    perguntaAtual++;
-    renderizarQuizzEscolhido();
-}*/
-
-
 function escolherResposta(resposta) {
-    perguntaAtual++;
-    
     if (resposta.classList.contains("esbranquicado")) {
         return;
     }
@@ -108,8 +99,15 @@ function escolherResposta(resposta) {
     const listaRespostas = document.querySelectorAll(".container-resposta").forEach(respostas => {
         respostas.classList.add("esbranquicado");
     })
-    
+
     resposta.classList.remove("esbranquicado");
+
+    const cartaClicada = resposta.querySelector("span").innerText;
+
+    perguntaAtual++;
+
+    verificarRespostaCerta(cartaClicada);
+    aplicarEstiloLegenda();
 }
 
 
@@ -127,4 +125,21 @@ function mostrarTela2() {
 
 function comparador() {
     return Math.random() - 0.5;
+}
+
+function verificarRespostaCerta(cartaClicada) {
+
+    const respostaCorreta = respostas
+        .find(resposta => resposta.isCorrectAnswer === true).text
+
+    if (cartaClicada === respostaCorreta) {
+        acertos++;
+    }
+
+}
+
+function aplicarEstiloLegenda() {
+    const legendas = document.querySelectorAll(".legenda").forEach(respostas => {
+        respostas.classList.add("erro");
+    })
 }
